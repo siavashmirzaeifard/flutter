aef7688d97e3c013aa8c063dd8b00b9164c12acf
cyafard <aminemadi.04@gmail.com>
2021-01-12 15:31:24 +0330

    test

--- flutter_calendar/lib/main.dart
+++ flutter_calendar/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.red,
+        color: Colors.blue,
       ),
     );
   }
